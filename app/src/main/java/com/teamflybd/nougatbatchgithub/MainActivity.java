d78f583a361862f5fa7239f3a2b8aeb7a42c21cc
Adil <https://_redemption@bitbucket.org/_redemption/lu-canteen.git>
2018-05-30 13:15:36 +0600

    Menu iflater & backPressed functionality implemented in the project.

--- app/src/main/java/com/teamflybd/nougatbatchgithub/MainActivity.java
+++ app/src/main/java/com/teamflybd/nougatbatchgithub/MainActivity.java
@@ -1,12 +1,19 @@
 package com.teamflybd.nougatbatchgithub;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+
 /*
 Nougat batch
 Acticity lifecycle
 28 May 2018
+
+BackPress
+Menus
 */
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Action bar*/
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        /** ActionBar */
         Log.i("I am in :", "onCreate");
     }
 
@@ -52,5 +67,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i("I am in :", "onDestroyed");
+    }
+
+
+    /*Adding functionality to back button*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/teamflybd/nougatbatchgithub/ProgressDialogActivity.java
+++ app/src/main/java/com/teamflybd/nougatbatchgithub/ProgressDialogActivity.java
@@ -2,12 +2,19 @@ package com.teamflybd.nougatbatchgithub;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-/*Progress dialog*/
+/*Progress dialog
+* Menus*/
 
 public class ProgressDialogActivity extends AppCompatActivity {
 
@@ -19,6 +26,15 @@ public class ProgressDialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_dialog);
+
+        /** ActionBar This is back button*/
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        /** ActionBar */
 
         //Typecasting does not require in updated version of android studio
         button = findViewById(R.id.button);
@@ -36,5 +52,47 @@ public class ProgressDialogActivity extends AppCompatActivity {
 
 
 
+
+
+
+    }
+
+    /** Creating menu by inflate*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if(id == android.R.id.home)
+//        {
+//            onBackPressed();
+//            //Toast.makeText(getApplicationContext(),"To sign out tap on action bar!",Toast.LENGTH_LONG).show();
+//            return true;
+//        }
+
+        int id = item.getItemId();
+
+        if( id == R.id.about)
+        {
+            Toast.makeText(this, "Clicked on about item", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.logout)
+        {
+            Toast.makeText(this, "Clicked on logout item", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(id == android.R.id.home)
+        {
+           // Toast.makeText(this, "Pressed on back button", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(ProgressDialogActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
